5f77a893121608c6c7d79c1eefc78aed64693591
mikerah1 <mikerah14@gmail.com>
2016-06-13 19:54:38 -0400

    Added skeleton code for the project

---
+++ app/src/main/java/com/mikerah/android/ancientmessageencryptor/Cipher.java
@@ -0,0 +1,10 @@
+package com.mikerah.android.ancientmessageencryptor;
+
+/**
+ * Created by Mikerah on 2016-06-13.
+ */
+public interface Cipher {
+
+    public String encrypt();
+    public String decrypt();
+}
---
+++ app/src/main/java/com/mikerah/android/ancientmessageencryptor/TranspositionCipher.java
@@ -0,0 +1,16 @@
+package com.mikerah.android.ancientmessageencryptor;
+
+/**
+ * Created by Mikerah on 2016-06-13.
+ */
+public class TranspositionCipher implements Cipher {
+    @Override
+    public String encrypt() {
+        return null;
+    }
+
+    @Override
+    public String decrypt() {
+        return null;
+    }
+}
