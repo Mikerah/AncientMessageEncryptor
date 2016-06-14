49996a9a174f732ff0964e4dfa8fc13d9a67f8e7
mikerah1 <mikerah14@gmail.com>
2016-06-14 16:13:28 -0400

    Decided to not create the AlbertiCipher and completed the encryption for the AffineCipher

--- app/src/main/java/com/mikerah/android/ancientmessageencryptor/AffineCipher.java
+++ app/src/main/java/com/mikerah/android/ancientmessageencryptor/AffineCipher.java
@@ -7,11 +7,34 @@ public class AffineCipher implements Cipher {
 
     @Override
     public String encrypt(String key, String message) {
-        return null;
+        String translated = null;
+        int encryptionKey = Integer.parseInt(key);
+        int keyA = (int) Math.floor(encryptionKey / 26);
+        int keyB = encryptionKey % 26;
+
+        for(int i = 0; i < message.length(); i++) {
+            if(Character.isLetter(message.charAt(i))) {
+                int index = 0;
+                if (Character.isUpperCase(message.charAt(i))) {
+                    index = message.charAt(i) - 'A';
+                    translated += (index + 65);
+                }
+                else if (Character.isLowerCase(message.charAt(i))) {
+                    index = message.charAt(i) - 'a';
+                    translated += (index + 97);
+                }
+                else {
+                    translated += message.charAt(i);
+                }
+            }
+        }
+
+        return translated;
     }
 
     @Override
     public String decrypt(String key, String message) {
-        return null;
+        String translated = null;
+        return translated;
     }
 }
