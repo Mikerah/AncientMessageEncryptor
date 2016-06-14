8ac8580f07fa8f5a0aeade177a6720a0fef66155
mikerah1 <mikerah14@gmail.com>
2016-06-14 08:52:41 -0400

    Completed Caesar Cipher

--- app/src/main/java/com/mikerah/android/ancientmessageencryptor/CaesarCipher.java
+++ app/src/main/java/com/mikerah/android/ancientmessageencryptor/CaesarCipher.java
@@ -6,11 +6,90 @@ package com.mikerah.android.ancientmessageencryptor;
 public class CaesarCipher implements Cipher {
     @Override
     public String encrypt(String key, String message) {
-        return null;
+        String translated = null;
+        int encryptionKey = Integer.parseInt(key);
+
+        for(int i = 0; i < translated.length(); i++) {
+            if(Character.isLetter(message.charAt(i))) {
+                if(Character.isUpperCase(message.charAt(i))) {
+                    int num = message.charAt(i) - 'A';
+                    num += encryptionKey;
+
+                    if (num >= 26) {
+                        num -= 26;
+                    }
+                    else {
+                        num += 26;
+                    }
+
+                    translated += (num + 65);
+                }
+                else {
+                    int num = message.charAt(i) - 'a';
+                    num += encryptionKey;
+
+                    if (num >= 26) {
+                        num -= 26;
+                    }
+                    else {
+                        num += 26;
+                    }
+
+                    translated += (num + 97);
+                }
+
+            }
+            else {
+                translated += message.charAt(i);
+            }
+
+        }
+
+        return translated;
+
     }
 
     @Override
     public String decrypt(String key, String message) {
-        return null;
+        String translated = null;
+        int encryptionKey = Integer.parseInt(key);
+
+        for(int i = 0; i < translated.length(); i++) {
+            if(Character.isLetter(message.charAt(i))) {
+                if(Character.isUpperCase(message.charAt(i))) {
+                    int num = message.charAt(i) - 'A';
+                    num -= encryptionKey;
+
+                    if (num >= 26) {
+                        num -= 26;
+                    }
+                    else {
+                        num += 26;
+                    }
+
+                    translated += (num + 65);
+                }
+                else {
+                    int num = message.charAt(i) - 'a';
+                    num += encryptionKey;
+
+                    if (num >= 26) {
+                        num -= 26;
+                    }
+                    else {
+                        num += 26;
+                    }
+
+                    translated += (num + 97);
+                }
+
+            }
+            else {
+                translated += message.charAt(i);
+            }
+
+        }
+
+        return translated;
     }
 }
