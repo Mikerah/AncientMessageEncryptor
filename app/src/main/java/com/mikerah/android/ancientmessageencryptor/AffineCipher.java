ad0fdd71604106aeee8679c1658c356a27bdc683
mikerah1 <mikerah14@gmail.com>
2016-06-14 16:26:19 -0400

    Completed AffineCipher

--- app/src/main/java/com/mikerah/android/ancientmessageencryptor/AffineCipher.java
+++ app/src/main/java/com/mikerah/android/ancientmessageencryptor/AffineCipher.java
@@ -17,15 +17,15 @@ public class AffineCipher implements Cipher {
                 int index = 0;
                 if (Character.isUpperCase(message.charAt(i))) {
                     index = message.charAt(i) - 'A';
-                    translated += (index + 65);
+                    translated += (((index * keyA + keyB) % 26) + 65);
                 }
                 else if (Character.isLowerCase(message.charAt(i))) {
                     index = message.charAt(i) - 'a';
-                    translated += (index + 97);
+                    translated += (((index * keyA + keyB) % 26) + 97);
                 }
-                else {
-                    translated += message.charAt(i);
-                }
+            }
+            else {
+                translated += message.charAt(i);
             }
         }
 
@@ -35,6 +35,28 @@ public class AffineCipher implements Cipher {
     @Override
     public String decrypt(String key, String message) {
         String translated = null;
+        int decryptionKey = Integer.parseInt(key);
+        int keyA = (int) Math.floor(decryptionKey / 26);
+        int keyB = decryptionKey % 26;
+        int modInverse = 26 - (keyA % 26);
+
+        for(int i =0; i < message.length(); i++) {
+            if(Character.isLetter(message.charAt(i))) {
+                int index = 0;
+                if (Character.isUpperCase(message.charAt(i))) {
+                    index = message.charAt(i) - 'A';
+                    translated += (((index - keyB) * modInverse) % 26) + 65;
+                }
+                else if (Character.isLowerCase(message.charAt(i))) {
+                    index = message.charAt(i) - 'a';
+                    translated += (((index - keyB) * modInverse) % 26) + 97;
+                }
+            }
+            else {
+                translated += message.charAt(i);
+            }
+        }
+
         return translated;
     }
 }
