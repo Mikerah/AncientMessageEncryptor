2e8aba5f39eaa22f8adf2b21e5b846cb705c5601
mikerah1 <mikerah14@gmail.com>
2016-06-15 22:11:17 -0400

    Completed the basic ui for the main activity

--- app/src/main/java/com/mikerah/android/ancientmessageencryptor/AncientEncryptorActivity.java
+++ app/src/main/java/com/mikerah/android/ancientmessageencryptor/AncientEncryptorActivity.java
@@ -3,11 +3,11 @@ package com.mikerah.android.ancientmessageencryptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AncientEncryptorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ancient_encryptor);
     }
 }
