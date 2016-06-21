1e9e5873791026864f97038caaa008a8fff78090
mikerah1 <mikerah14@gmail.com>
2016-06-21 15:18:39 -0400

    Fixed bugs in the affine cipher

--- app/src/main/java/com/mikerah/android/ancientmessageencryptor/AffineCipher.java
+++ app/src/main/java/com/mikerah/android/ancientmessageencryptor/AffineCipher.java
@@ -1,5 +1,7 @@
 package com.mikerah.android.ancientmessageencryptor;
 
+import java.math.BigInteger;
+
 /**
  * Created by Mikerah on 2016-06-13.
  */
@@ -7,7 +9,7 @@ public class AffineCipher implements Cipher {
 
     @Override
     public String encrypt(String key, String message) {
-        String translated = null;
+        String translated = "";
         int encryptionKey = Integer.parseInt(key);
         int keyA = (int) Math.floor(encryptionKey / 26);
         int keyB = encryptionKey % 26;
@@ -17,11 +19,11 @@ public class AffineCipher implements Cipher {
                 int index = 0;
                 if (Character.isUpperCase(message.charAt(i))) {
                     index = message.charAt(i) - 'A';
-                    translated += (((index * keyA + keyB) % 26) + 65);
+                    translated += (char)(((index * keyA + keyB) % 26) + 65);
                 }
                 else if (Character.isLowerCase(message.charAt(i))) {
                     index = message.charAt(i) - 'a';
-                    translated += (((index * keyA + keyB) % 26) + 97);
+                    translated += (char)(((index * keyA + keyB) % 26) + 97);
                 }
             }
             else {
@@ -34,22 +36,24 @@ public class AffineCipher implements Cipher {
 
     @Override
     public String decrypt(String key, String message) {
-        String translated = null;
+        String translated = "";
         int decryptionKey = Integer.parseInt(key);
         int keyA = (int) Math.floor(decryptionKey / 26);
         int keyB = decryptionKey % 26;
-        int modInverse = 26 - (keyA % 26);
+        int modInverse = new BigInteger(String.valueOf(keyA)).modInverse(BigInteger.valueOf(26)).intValue();
 
         for(int i =0; i < message.length(); i++) {
             if(Character.isLetter(message.charAt(i))) {
                 int index = 0;
                 if (Character.isUpperCase(message.charAt(i))) {
                     index = message.charAt(i) - 'A';
-                    translated += (((index - keyB) * modInverse) % 26) + 65;
+                    char toAdd = (char) (((((index - keyB) * modInverse) % 26 + 26) % 26) + 65);
+                    translated += toAdd;
                 }
                 else if (Character.isLowerCase(message.charAt(i))) {
                     index = message.charAt(i) - 'a';
-                    translated += (((index - keyB) * modInverse) % 26) + 97;
+                    char toAdd = (char) (((((index - keyB) * modInverse) % 26 + 26) % 26) + 97);
+                    translated += toAdd;
                 }
             }
             else {
